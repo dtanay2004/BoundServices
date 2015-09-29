ac0348ea1994a9b88a2f826ed35a6a1749004616
dtanay2004 <tanaydey@Tanays-MacBook-Pro.local>
2015-09-29 00:48:32 -0600

    Android program on bound services

---
+++ app/src/main/java/com/example/tanaydey/tutorial43_boundservices/MyService.java
@@ -0,0 +1,38 @@
+package com.example.tanaydey.tutorial43_boundservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Binder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MyService extends Service {
+
+    private final IBinder tanaysBinder = new MyLocalBinder();
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return tanaysBinder;
+    }
+
+    public String getCurrentTime(){
+
+        SimpleDateFormat df=new SimpleDateFormat("HH:mm:ss",Locale.CANADA);
+        return (df.format(new Date()));
+    }
+
+    public class MyLocalBinder extends Binder{
+
+        MyService getService(){
+
+          return MyService.this;
+        }
+    }
+
+
+}
